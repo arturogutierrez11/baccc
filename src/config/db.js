dbd35f04dd289fb2239e908a1980cd354e958f7c
arturogutierrez11 <arturogutierrez@Arturos-MacBook-Pro.local>
2022-11-18 00:49:49 -0300

    n

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,48 @@ const { Sequelize } = require("sequelize");
 const { user, password, host, database } = require("../utils/config.js");
 // const User = require("../models/User.js");
 // const Properties = require("../models/Property.js");
+const {
+  PGDATABASE,
+  PGHOST,
+  PGPORT,
+  PGUSER,
+  PGPASSWORD,
+  LOCAL_USER,
+  LOCAL_PASSWORD,
+  LOCAL_HOST,
+  DB_NAME,
+} = process.env;
+// const User = require("../models/User.js");
+// const Properties = require("../models/Property.js");
 
-const sequelize = new Sequelize(
-  `postgres://${user}:${password}@${host}:5432/${database}`,
-  {
-    logging: false,
-  }
-);
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: PGDATABASE,
+        dialect: "postgres",
+        host: PGHOST,
+        port: PGPORT,
+        username: PGUSER,
+        password: PGPASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: { ssl: {
+          require: true,
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+  : new Sequelize(
+      `postgres://${LOCAL_USER}:${LOCAL_PASSWORD}@${LOCAL_HOST}/${DB_NAME}`,
+      {
+        logging: false, // set to console.log to see the raw SQL queries
+        native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      }) 
 
 module.exports = sequelize;
